438a3158003001f458eadf38f4632c0d8219d09c
Artem Bilan <abilan@pivotal.io>
2016-01-05 19:11:48 -0500

    INTEXT-217: Cleanup KafkaProducerContext.beanName
    JIRA: https://jira.spring.io/browse/INTEXT-217
    
    The `KafkaProducerMessageHandlerSpec` provides an internal `KafkaProducerContext` for the bean registration.
    The `KafkaProducerContext` is `NamedComponent` and we don't generate `beanName` for it because its value is `not_specified` by default.
    That causes the `duplicate bean` issue in case of several ` Kafka.outboundChannelAdapter()`
    
    * Clean the `KafkaProducerContext.beanName` in the `KafkaProducerMessageHandlerSpec` to allow `IntegrationFlowBeanPostProcessor` to generate
    an unique `beanName` for each `KafkaProducerContext` bean definition.

--- src/main/java/org/springframework/integration/dsl/kafka/KafkaProducerMessageHandlerSpec.java
+++ src/main/java/org/springframework/integration/dsl/kafka/KafkaProducerMessageHandlerSpec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors
+ * Copyright 2015-2016 the original author or authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,6 +60,7 @@ public class KafkaProducerMessageHandlerSpec
 	KafkaProducerMessageHandlerSpec(Properties producerProperties) {
 		this.producerProperties = producerProperties;
 		this.kafkaProducerContext = new KafkaProducerContext();
+		this.kafkaProducerContext.setBeanName(null);
 		this.target = new KafkaProducerMessageHandler(kafkaProducerContext);
 	}
 
